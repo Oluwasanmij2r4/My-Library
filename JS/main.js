6708aebff1b0d01f1eaec83df92ac68d444383c7
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-01 21:34:50 +0100

    Add the logic for the add book button

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,11 @@
 const popBtn = document.querySelector(".addbtn")
 const popUp = document.querySelector("form")
 const cancel = document.querySelector(".cancel")
+const titleInput = document.querySelector("#title")
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const statusInput = document.querySelector("#status");
+const addButton = document.querySelector("#addBtn");
 
 popBtn.addEventListener("click", () => {
     popUp.classList.add('active')
@@ -10,3 +15,85 @@ popBtn.addEventListener("click", () => {
 cancel.addEventListener("click", () => {
     popUp.classList.remove('active')
 })
+
+const myLibrary = [];
+
+// constructor for books
+
+function myBooks (title, author, pages, status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+// function to create book
+
+function createBook(title, author, pages, status){
+    const newBook = new myBooks(title, author, pages, status);
+    myLibrary.push(newBook);
+}
+
+createBook("Pride and Prejudice", "Jane Austen", 279, true);
+createBook("The Great Gatsby", "F. Scott Fitzgerald", 180, false);
+createBook("The Hobbit", "J.R.R. Tolkien", 310, true);
+createBook("1984", "George Orwell", 328, false);
+createBook("To Kill a Mockingbird", "Harper Lee", 281, true);
+
+
+// display book function
+
+const displayBook = () => {
+    const container = document.querySelector('.container')
+    container.innerHTML = '';
+
+    for(let i = 0; i < myLibrary.length; i++){
+        const card = document.createElement('div');
+        const title = document.createElement('h2')
+        const author = document.createElement('p')
+        const pages = document.createElement("p");
+        const status = document.createElement("p");
+        const delbtn = document.createElement('button')
+
+        card.classList.add('book-card')
+        delbtn.classList.add('del-btn')
+
+        title.textContent = `Title: ${myLibrary[i].title}`
+        author.textContent = `Author: ${myLibrary[i].author}`;
+        pages.textContent = `Pages: ${myLibrary[i].pages}`;
+        status.textContent = `Status: ${myLibrary[i].status ? 'Read' : 'Not Read'}`;
+        delbtn.textContent = "Delete";
+
+        delbtn.addEventListener('click', () => {
+            myLibrary.splice(i,1);
+            displayBook();
+        });
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(status);
+        card.appendChild(delbtn);
+
+        container.appendChild(card)
+    }
+}
+
+displayBook()
+
+// Add Book through the Pop-up
+
+addButton.addEventListener('click', () => {
+    const title = titleInput.value.trim();
+    const author = authorInput.value.trim();
+    const pages = pagesInput.value.trim();
+    const status = statusInput.value;
+
+    const booleonStatus = status === 'Read';
+
+
+    createBook(title, author, pages, booleonStatus);
+    displayBook();
+
+    popUp.classList.remove("active");
+});
